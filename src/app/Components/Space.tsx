b5fdb0c9dabf0d2e81e1b5b1468f9bbdd4102c5a
Edmar Alencar <edmaaralencar1@gmail.com>
2024-05-20 11:11:18 -0300

    Corrigir tamanho de vídeo

--- src/app/Components/Space.tsx
+++ src/app/Components/Space.tsx
@@ -15,8 +15,8 @@ export function Space() {
       id="espaco"
     >
       <div id="video" className="mx-auto grid items-center md:py-0 py-8 grid-cols-1 md:grid-cols-[280px_1fr] gap-14 w-full max-w-6xl px-4">
-        <div className="h-[400px] md:h-[600px] md:mx-0 mx-auto w-[200px] md:w-[280px] bg-red-500 rounded-3xl border-[8px] relative border-black">
-          <div className="top-0 bg-black rounded-b-3xl mx-auto h-8 w-2/3 flex justify-center items-center gap-1">
+        <div className="h-[400px] md:h-[600px] md:mx-0 mx-auto w-[200px] md:w-[280px] rounded-3xl border-[8px] border-black relative overflow-hidden">
+          <div className="bg-black relative rounded-b-3xl mx-auto h-8 w-2/3 flex justify-center items-center gap-1 z-50">
             <div className="h-2 bg-gray-400 w-8 rounded-full"></div>
             
             <div className="h-2 bg-gray-400 w-3 rounded-full"></div>
@@ -24,6 +24,7 @@ export function Space() {
           </div>
           <video 
             controls
+            className='absolute object-fill top-0 bottom-0 w-full h-full'
           >
             <source src="/images/welcome.mp4" type="video/mp4" />
             Your browser does not support the video tag.
