2c8c35dd807ff86a88898c3b056f5f115b4ea7f1
TIAGO AMERICANO RODRIGUES <tiago.americano.03@gmail.com>
2024-05-20 18:09:08 -0300

    feat: final version

--- src/app/Components/Carousel.tsx
+++ src/app/Components/Carousel.tsx
@@ -27,6 +27,11 @@ export function CarouselSize() {
       '/images/img2.jpg',
       '/images/img3.jpg',
       '/images/img4.jpg',
+      '/images/img5.jpg',
+      '/images/img6.jpg',
+      '/images/img7.jpg',
+      '/images/img8.jpg',
+      '/images/img9.jpg',
     ];
 
     const loadImages = async () => {
