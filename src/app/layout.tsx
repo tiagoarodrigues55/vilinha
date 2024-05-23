cbd55138d059be4d779cb8a2261631e773205296
TIAGO AMERICANO RODRIGUES <tiago.americano.03@gmail.com>
2024-05-23 11:28:18 -0300

    feat: change components for Components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {WhatsappButton} from "./components/WhatsappButton";
+import {WhatsappButton} from "./Components/WhatsappButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
